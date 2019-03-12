550a9d061d6418ee1c4196c6275ca10583d8ef26
Daryl Titcomb <titcombd@seischeck.com>
2019-03-12 17:47:26 +0000

    now using 2 numbers and guesses
    - added second number to random generated list
    - now taking 2 guesses as list
    - more detailed output

--- Main.py
+++ Main.py
@@ -1,17 +1,34 @@
 import random
 
 RandNumList = []
+GuessList = []
 
 print ("Welcome to MasterMind")
 
-RandNumList.append(random.randint(1,8))
+for r in range(2):
+    RandNumList.append(random.randint(1,8))
 
 for q in RandNumList:
     print (q)
 
-Guess = input("Pick a number between 1 & 8: ")
+for r in range(2):
+    TempGuessInput = input("Pick a number between 1 & 8: ")  #fix later, none int values cause ValueError
+    GuessList.append(int(TempGuessInput))
 
-if int(Guess) == RandNumList[0]:               #fix later, none int values cause ValueError
-    print ("Correct!")
+for x in GuessList:
+    print (x)
+
+if GuessList[0] == RandNumList[0]:
+    print ("Guess 0 is correct!")
+elif GuessList[0] == RandNumList[1]:
+    print ("Guess 0 is correct, but not in the right place")
 else:
-    print ("Sorry that's not correct.")
+    print ("Sorry, guess 0 is not correct.")
+
+
+if GuessList[1] == RandNumList[0]:
+    print ("Guess 1 is correct, but not in the right place")
+elif GuessList[1] == RandNumList[1]:
+    print ("Guess 1 is correct!")
+else:
+    print ("Sorry, guess 1 is not correct.")
