ef92f8936d2727e7c670b123b92e582d0361c96b
Daryl Titcomb <titcombd@seischeck.com>
2019-03-19 17:13:05 +0000

    added repeat guesses
    program now loops guesses until user guesses all digits correctly

--- Main.py
+++ Main.py
@@ -11,24 +11,29 @@ for r in range(2):
 for q in RandNumList:
     print (q)
 
-for r in range(2):
-    TempGuessInput = input("Pick a number between 1 & 8: ")  #fix later, none int values cause ValueError
-    GuessList.append(int(TempGuessInput))
+while GuessList != RandNumList:
+    GuessList = []
 
-for x in GuessList:
-    print (x)
+    for r in range(2):
+        TempGuessInput = input("Pick a number between 1 & 8: ")  #fix later, none int values cause ValueError
+        GuessList.append(int(TempGuessInput))
 
-if GuessList[0] == RandNumList[0]:
-    print ("Guess 0 is correct!")
-elif GuessList[0] == RandNumList[1]:
-    print ("Guess 0 is correct, but not in the right place")
-else:
-    print ("Sorry, guess 0 is not correct.")
+    for x in GuessList:
+        print (x)
+    
+    #change output from guesses
+    if GuessList[0] == RandNumList[0]:
+        print ("Guess 0 is correct!")
+    elif GuessList[0] == RandNumList[1]:
+        print ("Guess 0 is correct, but not in the right place")
+    else:
+        print ("Sorry, guess 0 is not correct.")
 
-
-if GuessList[1] == RandNumList[0]:
-    print ("Guess 1 is correct, but not in the right place")
-elif GuessList[1] == RandNumList[1]:
-    print ("Guess 1 is correct!")
-else:
-    print ("Sorry, guess 1 is not correct.")
+    if GuessList[1] == RandNumList[0]:
+        print ("Guess 1 is correct, but not in the right place")
+    elif GuessList[1] == RandNumList[1]:
+        print ("Guess 1 is correct!")
+    else:
+        print ("Sorry, guess 1 is not correct.")
+    
+print ("thanks for playing")
